4c2125807c1badc02a5f4a50dea1e0179b31673c
Ulrich Petri <ulo@ulo.pe>
2015-04-13 17:09:11 -0400

    Add generate task

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,15 @@
-from fabric.api import put, task, cd, env
+from fabric.api import put, task, cd, env, local
 from fabric.contrib.project import rsync_project
-
 
 env.hosts = ['pyformat@pyformat.info']
 
 
 @task
+def generate():
+    local("pyformat generate")
+
+
+@task(default=True)
 def deploy():
     with cd('/var/www/pyformat.info'):
         put('index.html', './')
