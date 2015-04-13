f5ff5e436ae5a20a8017b33165b40c16858ace7f
Ulrich Petri <ulo@ulo.pe>
2015-04-13 14:45:20 -0400

    More examples

--- tests/test_content.py
+++ tests/test_content.py
@@ -4,11 +4,89 @@ def test_simple():
 
     ...
     """
-    old_result = '%s' % ('test', )
-    new_result = '{}'.format('test')
+    old_result = '%s' % ('hodor', )
+    new_result = '{}'.format('hodor')
 
     assert old_result == new_result
-    assert old_result == 'test'  # output
+    assert old_result == 'hodor'  # output
+
+
+def test_string_pad_align_right():
+    """
+    Pad with spaces and align right
+
+    ...
+    """
+    old_result = '%10s' % ('test', )
+    new_result = '{:>10}'.format('test')
+
+    assert old_result == new_result
+    assert old_result == '      test'  # output
+
+
+def test_string_pad_align_left():
+    """
+    Pad with spaces and align left
+
+    ...
+    """
+    old_result = '%-10s' % ('test', )
+    new_result = '{:10}'.format('test')
+
+    assert old_result == new_result
+    assert old_result == 'test      '  # output
+
+
+def test_string_truncating():
+    """
+    Truncate overly long strings
+
+    ...
+    """
+    old_result = '%5.5s' % ('xylophone', )
+    new_result = '{:5.5}'.format('xylophone')
+
+    assert old_result == new_result
+    assert old_result == 'xylop'  # output
+
+
+def test_integer():
+    """
+    Format interger
+
+    ...
+    """
+    old_result = '%d' % (42, )
+    new_result = '{:d}'.format(42)
+
+    assert old_result == new_result
+    assert old_result == '42'  # output
+
+
+def test_integer_padding_zero():
+    """
+    Format interger with zero padding
+
+    ...
+    """
+    old_result = '%04d' % (42, )
+    new_result = '{:04d}'.format(42)
+
+    assert old_result == new_result
+    assert old_result == '0042'  # output
+
+
+def test_integer_padding_space():
+    """
+    Format interger with space padding
+
+    ...
+    """
+    old_result = '%4d' % (42, )
+    new_result = '{:4d}'.format(42)
+
+    assert old_result == new_result
+    assert old_result == '  42'  # output
 
 
 def test_dictionary_access():
