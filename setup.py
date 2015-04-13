05b5a618fa8e14b94bc5f2b4d96ae5dad2a0a485
Ulrich Petri <ulo@ulo.pe>
2015-04-13 13:43:58 -0400

    Add entry point

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='pyformat_site'
+    name='pyformat_site',
+    entry_points={
+        'console_scripts': [
+            'pyformat = main:main',
+        ]
+    },
 )
