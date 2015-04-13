529084afa7f00e68c53e1fd3475fb94dbdda5152
Ulrich Petri <ulo@ulo.pe>
2015-04-13 13:43:08 -0400

    Add simple example

--- tests/test_content.py
+++ tests/test_content.py
@@ -1,3 +1,16 @@
+def test_simple():
+    """
+    Simple formatting
+
+    ...
+    """
+    old_result = '%s' % ('test', )
+    new_result = '{}'.format('test')
+
+    assert old_result == new_result
+    assert old_result == 'test'  # output
+
+
 def test_dictionary_access():
     """
     Dictionary Access
