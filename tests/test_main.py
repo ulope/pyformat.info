e347e438d15e18ae395f13b88fe64c5b25b09726
Horst Gutmann <zerok@zerokspot.com>
2015-04-14 12:14:02 -0400

    Cover CSS generation

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,48 @@
-from main import parse_docstring, parse_function
+from pathlib import Path
+from shutil import rmtree
+
+from main import parse_docstring
+from main import parse_function
+from main import split_letters
+from main import generate_css
+
+
+def test_split_letters():
+    assert split_letters('a bc') == '<i>a</i><i> </i><i>b</i><i>c</i>'
+
+
+def test_generate_css_creates_output_folder():
+    here = Path(__file__).parent
+    fixture_input = here / 'fixtures' / 'css' / 'sass'
+    fixture_output = here / 'fixtures' / 'css' / 'css'
+    if fixture_output.exists():
+        rmtree(str(fixture_output))
+    assert not fixture_output.exists()
+    generate_css(fixture_input, fixture_output)
+    assert fixture_output.exists()
+
+
+def test_generate_css_output_folder_exists():
+    """
+    If the output folder already exists, the code should use it.
+    """
+    here = Path(__file__).parent
+    fixture_input = here / 'fixtures' / 'css' / 'sass'
+    fixture_output = here / 'fixtures' / 'css' / 'css'
+    if not fixture_output.exists():
+        fixture_output.mkdir(parents=True)
+    assert fixture_output.exists()
+    generate_css(fixture_input, fixture_output)
+    assert fixture_output.exists()
+
+
+def test_generate_css(tmpdir):
+    here = Path(__file__).parent
+    fixture_input = here / 'fixtures' / 'css' / 'sass'
+    fixture_output = Path(str(tmpdir))
+    mapping = generate_css(fixture_input, fixture_output)
+    assert (fixture_output / 'style.cf83e135.css').exists()
+    assert mapping == {'style.scss': 'style.cf83e135.css'}
 
 
 def test_parse_docstring_without_docstring():
