48763cfa8d9f6e2ef754634b1e07d8fa6b87338a
Horst Gutmann <zerok@zerokspot.com>
2015-04-14 15:12:30 -0400

    Compressed CSS and sourcemaps

--- main.py
+++ main.py
@@ -20,11 +20,21 @@ Example = namedtuple("Example", ('name', 'title', 'details', 'setup', 'old', 'ne
 
 
 def compile_sass(source_path, target_path_pattern):
-    output = sass.compile(filename=str(source_path))
+    # First generate the content from which we can generate the hashname
+    output = sass.compile(
+        filename=str(source_path),
+        output_style='compressed')
     hash = hashlib.sha512(output.encode('utf-8')).hexdigest()[:8]
     target_path = str(target_path_pattern).format(hash)
+    source_map_target_path = target_path + '.map'
+    output = sass.compile(
+        filename=str(source_path),
+        output_style='compressed',
+        source_map_filename=source_map_target_path)
     with open(target_path, 'w') as fp:
-        fp.write(output)
+        fp.write(output[0])
+    with open(source_map_target_path, 'w') as fp:
+        fp.write(output[1])
     return Path(target_path)
 
 
