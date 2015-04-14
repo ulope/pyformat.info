9663341ff2df4a11e7a9420323bfacf573da2e45
Horst Gutmann <zerok@zerokspot.com>
2015-04-14 15:58:35 -0400

    Fix some descriptions

--- tests/test_content.py
+++ tests/test_content.py
@@ -44,11 +44,11 @@ def test_string_pad_align():
 
     By default values are formatted to take up only as many characters as
     needed to represent the content. It is however also possible to define that
-    a value should be padded by a specific amount.
+    a value should be padded to a specific length.
 
     Unfortunately the default alignment differs between old and new style
-    formatting. Old style defaults to right aligned while for new style it's
-    left.
+    formatting. The old style defaults to right aligned while for new style
+    it's left.
 
     Align right:
     """
