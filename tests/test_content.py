a1623f5b8c673ec8f829e4a7f484c9abe51e9f34
Ulrich Petri <ulo@ulo.pe>
2015-04-14 11:56:40 -0400

    Fix typos

--- tests/test_content.py
+++ tests/test_content.py
@@ -4,11 +4,11 @@ def test_simple():
 
     Simple positional formatting is probably the most common use-case. Use it
     if the order of your arguments is not likely to change and you only have
-    very few elements you want to concatinate.
+    very few elements you want to concatenate.
 
     Since the elements are not represented by something as descriptive as a
     name it is easy to confuse their positions in the format if you try to
-    concatinate too many elements.
+    concatenate too many elements.
     """
     old_result = '%s %s' % ('one', 'two', )
     new_result = '{} {}'.format('one', 'two')
