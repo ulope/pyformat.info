7c3022d4d9022ee5066fbc63748a8a79f60aa5d6
Ulrich Petri <ulo@ulo.pe>
2015-06-02 14:07:17 +0100

    Updated requirements

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
         "click==4.0",
         "jinja2==2.7.3",
         "python-rex==0.4",
-        "libsass==0.7.0",
-        "Markdown==2.6.1",
-        "pytest==2.7.0",
+        "libsass==0.8.2",
+        "Markdown==2.6.2",
+        "pytest==2.7.1",
         "Pygments==2.0.2",
         "astunparse==1.2.2",
-        "pytz==2015.2",
+        "pytz==2015.4",
     ],
     entry_points={
         'console_scripts': [
